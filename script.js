92ce0047a64f08307f683e3f72bd2c740d3f3ca8
Little Bagsuu <114639765+GodzGit@users.noreply.github.com>
2023-09-05 09:41:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.nav-toggle')
+const navMenu = document.querySelector('.nav-menu')
+
+navToggle.addEventListener('click',  () => {
+    navMenu.classList.toggle('active');
+});
+
+// <div class="tenor-gif-embed" data-postid="22279065" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/asta-black-clover-black-clover-asta-gif-22279065">Asta Black Clover GIF</a>from <a href="https://tenor.com/search/asta-gifs">Asta GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
